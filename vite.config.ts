468d1e0ceca1ec9802594c272234fe95320b4282
Nikhil dangi <nikhildangi1205@gmail.com>
2025-12-15 18:39:08 +0530

    deploy react

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base:"/My-portfolio",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
